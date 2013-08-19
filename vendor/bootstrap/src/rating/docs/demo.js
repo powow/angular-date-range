b26a7b006124767eeef5bffa53de4e00b4b1a936
Adam Pohorecki <adam.pohorecki@llp.pl>
2013-08-19 11:38:51 +0200

    Added angular-ui boostrap

---
+++ vendor/bootstrap/src/rating/docs/demo.js
@@ -0,0 +1,10 @@
+var RatingDemoCtrl = function ($scope) {
+  $scope.rate = 7;
+  $scope.max = 10;
+  $scope.isReadonly = false;
+
+  $scope.hoveringOver = function(value) {
+    $scope.overStar = value;
+    $scope.percent = 100 * (value / $scope.max);
+  };
+};
---
+++ vendor/bootstrap/src/rating/rating.js
@@ -0,0 +1,58 @@
+angular.module('ui.bootstrap.rating', [])
+
+.constant('ratingConfig', {
+  max: 5
+})
+
+.controller('RatingController', ['$scope', '$attrs', '$parse', 'ratingConfig', function($scope, $attrs, $parse, ratingConfig) {
+
+  this.maxRange = angular.isDefined($attrs.max) ? $scope.$parent.$eval($attrs.max) : ratingConfig.max;
+
+  $scope.range = [];
+  for (var i = 1; i <= this.maxRange; i++) {
+    $scope.range.push(i);
+  }
+
+  $scope.rate = function(value) {
+    if ( ! $scope.readonly ) {
+      $scope.value = value;
+    }
+  };
+
+  $scope.enter = function(value) {
+    if ( ! $scope.readonly ) {
+      $scope.val = value;
+    }
+    $scope.onHover({value: value});
+  };
+
+  $scope.reset = function() {
+    $scope.val = angular.copy($scope.value);
+    $scope.onLeave();
+  };
+
+  $scope.$watch('value', function(value) {
+    $scope.val = value;
+  });
+
+  $scope.readonly = false;
+  if ($attrs.readonly) {
+    $scope.$parent.$watch($parse($attrs.readonly), function(value) {
+      $scope.readonly = !!value;
+    });
+  }
+}])
+
+.directive('rating', function() {
+  return {
+    restrict: 'EA',
+    scope: {
+      value: '=',
+      onHover: '&',
+      onLeave: '&'
+    },
+    controller: 'RatingController',
+    templateUrl: 'template/rating/rating.html',
+    replace: true
+  };
+});
